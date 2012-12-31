7e49f0f6dfa3f778baf875cca4e11535f8326dd3
Mohammed Arabiat <tanjoodo@hotmail.com>
2012-12-31 19:36:12 +0300

    Update Question 10/ConsoleApplication10/Module1.vb

--- Question 10/ConsoleApplication10/Module1.vb
+++ Question 10/ConsoleApplication10/Module1.vb
@@ -6,7 +6,8 @@
         If (t >= 93) Then
             Console.WriteLine("WARNING: A value of 93 or more will cause the long unsigned integer to overflow, which causes security threats and also, it will" +
                               " break the program.")
-            Console.WriteLine("Press Enter to continue with this disaster, you monster...")
+            Console.WriteLine("Press Enter to continue with this disaster, you monster...") //Just wanted to point out it's (mostly) humorous and
+                                                                                            //it won't cause your computer to go down in flames.
             Console.ReadLine()
         End If
         Dim i As Integer = 0
